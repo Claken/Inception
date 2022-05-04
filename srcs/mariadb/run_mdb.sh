b3ee8ef39ea989858b61869c5e4f9efb6ef5063e
sachouam <sachouam@debian-BULLSEYE-live-builder-AMD64>
2022-05-04 12:25:40 -0400

    mariadb : script works : database created BUT mariadb container stops around 1mn after run

--- srcs/mariadb/run_mdb.sh
+++ srcs/mariadb/run_mdb.sh
@@ -1,7 +1,8 @@
-env
-set -xv
-service mysql start && echo "CREATE DATABASE $DB_NAME;" | mysql -u root --skip-password && \
+#!/bin/sh
+# env
+# set -xv
+service mysql start && echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql -u root --skip-password && \
 echo "CREATE USER '$THE_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';" | mysql -u root --skip-password && \
 echo "GRANT ALL ON $DB_NAME.* TO '$THE_USER'@'localhost';" | mysql -u root --skip-password && \
-echo "FLUSH PRIVILEGES;"
-exec mysqld_safe
+echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
+exec "$@"
