1d8c9f415074cb18e9036a131a32df34297967bb
sachouam <sachouam@debian-BULLSEYE-live-builder-AMD64>
2022-05-10 15:22:36 -0400

    makefile modified + add mkdir -p /run/php/ 2

--- srcs/mariadb/run_mdb.sh
+++ srcs/mariadb/run_mdb.sh
@@ -2,7 +2,7 @@
 # env
 set -xv
 service mysql start && echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql -u root --skip-password && \
-echo "CREATE USER '$THE_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';" | mysql -u root --skip-password && \
-echo "GRANT ALL ON $DB_NAME.* TO '$THE_USER'@'localhost';" | mysql -u root --skip-password && \
+echo "CREATE USER '$THE_USER'@'$DOMAIN_NAME' IDENTIFIED BY '$DB_PASSWORD';" | mysql -u root --skip-password && \
+echo "GRANT ALL ON $DB_NAME.* TO '$THE_USER'@'$DOMAIN_NAME';" | mysql -u root --skip-password && \
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password && service mysql stop
 exec "$@"
