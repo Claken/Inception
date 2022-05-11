56867b6fe0f84ad3cc5c0cf6959a169cf0eda837
sachouam <sachouam@debian-BULLSEYE-live-builder-AMD64>
2022-05-11 12:22:35 -0400

    problems with mysqld.sock : need to fix it

--- srcs/wordpress/run_wp.sh
+++ srcs/wordpress/run_wp.sh
@@ -3,6 +3,9 @@
 set -xv
 cd /var/www/html
 wp-cli core download --allow-root
+until mysqladmin -hmariadb -u$THE_USER -p$DB_PASSWORD ping && mariadb -hmariadb -u$THE_USER -p$DB_PASSWORD -e "SHOW DATABASES;" | grep "$DB_NAME"; do
+	sleep 2
+done
 wp-cli config create --dbname=$DB_NAME --dbuser=$THE_USER --dbpass=$DB_PASSWORD --locale=en_EN --allow-root
 wp-cli core install --url=$DOMAIN_NAME --title=Hello_World --admin_user=$THE_USER \
 --admin_password=$WP_ADM_PASSWORD --admin_email=$WP_ADM_EMAIL --allow-root
