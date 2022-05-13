025faf694ae7b93d103cf8a40c58547d3105201f
sachouam <sachouam@debian-BULLSEYE-live-builder-AMD64>
2022-05-13 15:50:41 -0400

    correction in ngin.conf + run_wp_sh changed = WORDPRESS WORKS

--- srcs/wordpress/run_wp.sh
+++ srcs/wordpress/run_wp.sh
@@ -2,6 +2,9 @@
 # cd /var/www/html/wordpress
 set -xv
 cd /var/www/html
+
+chmod 755 .
+
 wp-cli core download --allow-root
 until mysqladmin -hmariadb -u$THE_USER -p$DB_PASSWORD ping && mariadb -hmariadb -u$THE_USER -p$DB_PASSWORD -e "SHOW DATABASES;" | grep "$DB_NAME"; do
 	sleep 2
@@ -10,4 +13,7 @@ wp-cli config create --dbhost=mariadb --dbname=$DB_NAME --dbuser=$THE_USER --dbp
 wp-cli core install --url=$DOMAIN_NAME --title=Hello_World --admin_user=$THE_USER \
 --admin_password=$WP_ADM_PASSWORD --admin_email=$WP_ADM_EMAIL --allow-root
 wp-cli user create $WP_USER_NAME $WP_USER_EMAIL --role=subscriber --user_pass=$WP_USER_PASSWORD --allow-root
+
+rm index.nginx-debian.html
+
 exec "$@"
