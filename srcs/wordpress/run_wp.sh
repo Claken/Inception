d7e8bdbef1f93b3a6ba791c0eeb030b951ebc239
sachouam <sachouam@debian-BULLSEYE-live-builder-AMD64>
2022-05-13 14:13:10 -0400

    wp-config.php created + my.cnf file

--- srcs/wordpress/run_wp.sh
+++ srcs/wordpress/run_wp.sh
@@ -6,7 +6,7 @@ wp-cli core download --allow-root
 until mysqladmin -hmariadb -u$THE_USER -p$DB_PASSWORD ping && mariadb -hmariadb -u$THE_USER -p$DB_PASSWORD -e "SHOW DATABASES;" | grep "$DB_NAME"; do
 	sleep 2
 done
-wp-cli config create --dbname=$DB_NAME --dbuser=$THE_USER --dbpass=$DB_PASSWORD --locale=en_EN --allow-root
+wp-cli config create --dbhost=mariadb --dbname=$DB_NAME --dbuser=$THE_USER --dbpass=$DB_PASSWORD --locale=en_EN --allow-root
 wp-cli core install --url=$DOMAIN_NAME --title=Hello_World --admin_user=$THE_USER \
 --admin_password=$WP_ADM_PASSWORD --admin_email=$WP_ADM_EMAIL --allow-root
 wp-cli user create $WP_USER_NAME $WP_USER_EMAIL --role=subscriber --user_pass=$WP_USER_PASSWORD --allow-root
